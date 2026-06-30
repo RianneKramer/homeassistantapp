e748a57cb609c5d834a09e7063bad8dafd3ebc43
Rianne Kramer <715731@student.inholland.nl>
2026-06-30 15:33:34 +0200

    deployment patch

---
+++ backend/dashboard-api/Migrations/20260630133318_deployment patch.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dashboard_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class deploymentpatch : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Settings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "HomeAssistantUrl",
+                value: "http://192.168.2.29:8123");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Settings",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "HomeAssistantUrl",
+                value: "http://192.168.2.28:8123");
+        }
+    }
+}

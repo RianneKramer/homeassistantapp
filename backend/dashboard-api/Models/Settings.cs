f9fe914815795ff270def50ac43f02c39d3f2f7e
Rianne Kramer <715731@student.inholland.nl>
2026-06-30 11:44:35 +0200

    Backend: models

---
+++ backend/dashboard-api/Models/Settings.cs
@@ -0,0 +1,8 @@
+﻿namespace dashboard_api.Models;
+
+public class Settings
+{
+    public int Id { get; set; }
+    public string HomeAssistantUrl { get; set; } = string.Empty;
+    public string HomeAssistantToken { get; set; } = string.Empty;
+}
